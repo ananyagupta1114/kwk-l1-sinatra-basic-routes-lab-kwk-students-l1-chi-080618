64799f9dcc95df80ff18b39ab65c5d05b86b4fce
ananyagupta1114 <ananyagupta1114@gmail.com>
2018-08-14 16:51:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,5 +3,12 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/' do
-    ""
+    "My name is Ananya."
+  end
+  get "/" do
+  "My hometown is Chicago."
+  end
+  get '/' do
+  "My favorite song is Forever."
+  end
 end
